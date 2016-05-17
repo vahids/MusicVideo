d250b3cd3d6392c91c636645f1776b8cf7a07cd2
Vahid Sayad <vahidgiga@gmail.com>
2016-05-17 19:47:40 +0430

    Part 18 Completed. Detail view controller added

---
+++ MusicVideo/MusicVideoDetailVC.swift
@@ -0,0 +1,20 @@
+//
+//  MusicVideoDetailVC.swift
+//  MusicVideo
+//
+//  Created by Vahid Sayad on 5/17/16.
+//  Copyright © 2016 Vahid Sayad. All rights reserved.
+//
+
+import UIKit
+
+class MusicVideoDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
