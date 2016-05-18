ae801f9d5b331a3871441cb00b51879c30f2a59d
Vahid Sayad <vahidgiga@gmail.com>
2016-05-18 18:11:50 +0430

    Part 24 - Completed, Slider Part 2/3

--- MusicVideo/SettingsTVC.swift
+++ MusicVideo/SettingsTVC.swift
@@ -26,7 +26,39 @@ class SettingsTVC: UITableViewController {
 
         tableView.alwaysBounceVertical = false
         
+        touchID.on = NSUserDefaults.standardUserDefaults().boolForKey("SecuritySetting")
+        
+        title = "Settings"
+        
+        sliderCount.minimumValue = 10
+        sliderCount.maximumValue = 200
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        if let value = defaults.objectForKey("SliderValue") {
+            sliderCount.value =  value as! Float
+            APICount.text = "\(value)"
+        } else {
+            sliderCount.value = 10.0
+            APICount.text = "10"
         }
+        
+        
+        }
+    
+    
+    @IBAction func touchIDClick(sender: UISwitch) {
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        if touchID.on {
+            defaults.setBool(touchID.on, forKey: "SecuritySetting")
+        } else {
+            defaults.setBool(false, forKey: "SecuritySetting")
+        }
+        
+    }
+    
+    
+    
     
     func perefedFontChange(){
         feedbackDisplay.font = UIFont.preferredFontForTextStyle(UIFontTextStyleSubheadline)
@@ -40,5 +72,12 @@ class SettingsTVC: UITableViewController {
 
     deinit{
         NSNotificationCenter.defaultCenter().removeObserver(self, name: UIContentSizeCategoryDidChangeNotification, object: nil)
+    }
+    
+    @IBAction func sliderChange(sender: UISlider) {
+       // APICount.text = "\(sliderCount.value as Int)" 
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setObject(Int(sliderCount.value), forKey: "SliderValue")
+        APICount.text = "\(Int(sliderCount.value))"
     }
 }
